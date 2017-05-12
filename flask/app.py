6d8496471f49834c83ec6e29a00d328b16c8a455
Arvin Cudanin <arvin.cudanin@nokia.com>
2017-05-12 13:17:36 +0800

    fixed port

--- flask/app.py
+++ flask/app.py
@@ -13,4 +13,4 @@ def http():
     return "Successfully accessed normal HTTP!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
