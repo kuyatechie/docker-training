62eeb32a3ea6df15ef89190b5a216a3a3839a2bf
Arvin Cudanin <arvin.cudanin@gmail.com>
2019-02-18 11:07:08 +0800

    Fix proxies and port assignments

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,16 @@
 from flask import Flask
 import os
+import socket
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     name=str(os.environ["NAME"])
-    return "Hello {}!".format(name)
+    return "Hello {}. Welcome to Docker! From your Friend, {}!".format(name, socket.gethostbyname(socket.gethostname()))
 
 @app.route("/http-normal")
 def http():
     return "Successfully accessed normal HTTP!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
